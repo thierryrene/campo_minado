a67d610f16370270afca665ca99b2ee024abcae3
thierry rene matos <thierry.matos@stealthelook.com.br>
2017-10-06 14:08:09 -0300

    update

--- mine.php
+++ mine.php
@@ -45,7 +45,7 @@ require '../composer/vendor/autoload.php';
 <body>
 	
 
-	<div align="center" style="margin-top: 100px;">
+	<div align="center" style="margin-top: 60px;">
 
 		<h1>Mine</h1>
 
@@ -64,37 +64,52 @@ require '../composer/vendor/autoload.php';
 					session_start();
 
 					// valores iniciais
-					$linhas 		= 5;
-					$colunas 		= 5;
-					$bombas 		= 4;
+					$linhas 		= 6;
+					$colunas 		= 6;
+					$bombas 		= 8;
 					$bomba 			= "@";
 					$bombasMedia 	= ceil(($linhas * $colunas) / $bombas);
 
 					// valores url
-					$acao 			= !empty($_REQUEST['acao']) ? true : false;
+					$acao 			= !empty($_REQUEST['acao']) ? $_REQUEST['acao'] : '';
 					$clickLinha 	= !empty($_REQUEST['linha']) ? $_REQUEST['linha'] : 0;
 					$clickColuna 	= !empty($_REQUEST['coluna']) ? $_REQUEST['coluna'] : 0;
 
 					// contadores
-					$cellCount 				= 0;				
+					$cellCount = 0;				
 
 					// array que vai abrir a máscara da matriz que vamos utilizar
-					$firstMaskArray = array();
+					$onPageMask = array();
 
-					// criamos a matriz com base nas variáveis linhas e colunas
-					for ($a = 1; $a <= $linhas; $a++) {						
-						echo "<tr>";
-							for($b = 1; $b <= $colunas; $b++) {
-								echo "<td><a href='?acao=true&linha={$a}&coluna={$b}&celula={$cellCount}'>{$cellCount}</a></td>";
-								$firstMaskArray[$a][$b] = $cellCount;
-								$cellCount++;
-							}
-						echo "</tr>";
+					if ($acao == 'click') {
+
+						// criamos a matriz com base nas variáveis linhas e colunas
+						for ($a = 1; $a <= $linhas; $a++) {						
+							echo "<tr>";
+								for($b = 1; $b <= $colunas; $b++) {
+									echo "<td><a href='?acao=click&linha={$a}&coluna={$b}&celula={$cellCount}'>{$cellCount}</a></td>";
+									$onPageMask[$a][$b] = $cellCount;
+									$cellCount++;
+								}
+							echo "</tr>";
+						}
+					}					
+
+					$secondMask = array();
+
+					for ($a = 1; $a <= $colunas; $a++) {
+						$secondMask[$a] = $bomba;
 					}
 
-					// salvamos o jogo na sessão
-					// $_SESSION['firstM'] = $firstMaskArray;
-					// session_destroy($_SESSION);
+					$sortArray = array();
+
+					for($a = 1; $a <= $linhas; $a++) {
+						for($b = 1; $b <= $colunas; $b++) {
+							$sortArray[$a][$b] = '';
+						}
+					}
+
+					// shuffle($secondMask);
 					
 				?>
 
@@ -106,14 +121,20 @@ require '../composer/vendor/autoload.php';
 		<br>
 
 		<?php
+
 			// se houver ação de click, apresentar mensagem de qual célula foi clicada
-			if ($_GET['acao']) {
-				$linha 	= $_GET['linha'];
-				$coluna = $_GET['coluna'];
-				$celula = $_GET['celula'];
+			if ($_REQUEST['acao']) {
+
+				$linha 	= $_REQUEST['linha'];
+				$coluna = $_REQUEST['coluna'];
+				$celula = $_REQUEST['celula'];
+
 				echo "<span class='alert'>linha {$linha}, coluna {$coluna}, célula {$celula}</span>";
+
 				unset($linha, $coluna, $celula);
+
 			}
+			
 		?>
 
 	</div>
