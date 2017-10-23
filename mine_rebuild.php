caa6312176be723a8f17cdc0479dafcd6b529bf4
thierry rene matos <thierry.matos@stealthelook.com.br>
2017-10-23 20:03:00 -0200

    update

--- mine_rebuild.php
+++ mine_rebuild.php
@@ -2,12 +2,12 @@
 
 session_start();
 
-require_once '../testando_composer/vendor/autoload.php';    
+require_once '../composer/vendor/autoload.php';    
 
-$linhas  = 20;
+$linhas  = 10;
 // colunas com o mesmo valor das linhas
 $colunas = $linhas;
-$bombas = 10;
+$bombas = rand(1, 10);
 $bomba = '@';
 $totalCelulas = $linhas * $colunas;
 
@@ -52,7 +52,7 @@ if($acao != 'click') {
 	$matrizComBombas = $matrizInicial;
 
 	// utilizamos um contador das bombas para determinar o indícel que deve ser inserido na matriz com bombas
-	$contadorBombas = 0;
+	$contadorBombas = 1;
 
 	// distribuimos os valores do vetor na matriz
 	for ($l = 1; $l <= $linhas; $l++) {
@@ -62,40 +62,98 @@ if($acao != 'click') {
 		}
 	}
 
+	$bombaContador = 1;
+
 	foreach($matrizComBombas as $l => $linha) {
 		foreach ($linha as $c => $coluna) {
 			
 			if ($coluna == '@') {
 				
+				// bloco de cima
 				if (($l + 1) <= $linhas ) {
-					if ($matrizComBombas[$l + 1][$c] != '@') {
+					if ($matrizComBombas[$l + 1][$c] != $bomba) {
 						if ($matrizComBombas[$l + 1][$c] != 1) {
 							$matrizComBombas[$l + 1][$c] = 1;
 						}
+					} else {
+						$bombaContador++;
 					}
-				}
-				
+				}			
+					
+				// bloco da frente
 				if (($c + 1) <= $colunas ) {
-					if ($matrizComBombas[$l][$c + 1] != '@') {
+					if ($matrizComBombas[$l][$c + 1] != $bomba) {
 						if ($matrizComBombas[$l][$c + 1] != 1) {
 							$matrizComBombas[$l][$c + 1] = 1;
-						}
+						} 
+					} else {
+						$bombaContador++;
 					}
-				}
+				}				
 				
-				if (($l - 1) > 0 ) {
-					if ($matrizComBombas[$l - 1][$c] != '@') {
+				// bloco de baixo
+				if (($l - 1) > 0) {
+					if ($matrizComBombas[$l - 1][$c] != $bomba) {
 						if ($matrizComBombas[$l - 1][$c] != 1) { 
 							$matrizComBombas[$l - 1][$c] = 1;
-						}
+						} 
+					} else {
+						$bombaContador++;
 					}
 				}
 				
+				// bloco de trás
 				if (($c - 1) > 0 ) {
-					if ($matrizComBombas[$l][$c - 1] != '@') {
+					if ($matrizComBombas[$l][$c - 1] != $bomba) {
 						if ($matrizComBombas[$l][$c - 1] != 1) { 
 							$matrizComBombas[$l][$c - 1] = 1;
-						} 
+						}
+					} else {
+						$bombaContador++;
+					}
+				}
+
+				// diagonal inferior direita
+				if (($l + 1) <= $linhas && ($c + 1) <= $colunas) {
+					if ($matrizComBombas[$l + 1][$c + 1] != $bomba) {
+						if ($matrizComBombas[$l + 1][$c + 1] != 1) {
+							$matrizComBombas[$l + 1][$c + 1] = 1;
+						}
+					} else {
+						$bombaContador++;
+					}
+				}
+
+				// diagonal superior direita
+				if (($c + 1) <= $colunas && ($l - 1) > 0) {
+					if ($matrizComBombas[$l - 1][$c + 1] != $bomba) {
+						if ($matrizComBombas[$l - 1][$c + 1] != 1) {
+							$matrizComBombas[$l - 1][$c + 1] = 1;
+						}
+					} else {
+						$bombaContador++;
+					}
+				}
+
+				// diagonal superior esquerda
+				if (($c - 1) > 0 && ($l - 1) > 0) {
+					if ($matrizComBombas[$l - 1][$c - 1] != $bomba) {
+						if ($matrizComBombas[$l - 1][$c - 1] != 1) {
+							$matrizComBombas[$l - 1][$c - 1] = 1;
+						}
+					} else {
+						$bombaContador++;
+					}
+				}
+
+				// diagonal inferior esquerda
+				if (($l + 1) <= $linhas && ($c - 1) > 0 ) {
+					if ($matrizComBombas[$l + 1][$c - 1] != $bomba) {
+						if ($matrizComBombas[$l + 1][$c - 1] != 1) {
+							$matrizComBombas[$l + 1][$c - 1] = 1;
+						}
+					} else {
+						$bombaContador++;
 					}
 				}
 				
@@ -148,8 +206,13 @@ if($acao != 'click') {
 			<?php
 
 				foreach ($matrizComBombas as $l => $linha) {
+
 					echo "<tr>";
+
 						foreach($linha as $c => $coluna) {
+
+							// echo "<td style='background-color:grey;' onclick='javascript: window.location=\"mine_rebuild.php?acao=click&linha={$l}&coluna={$c}\"'>{$coluna}</td>";
+
 							if ($coluna == '') {
 								echo "<td style='background-color:grey;' onclick='javascript: window.location=\"mine_rebuild.php?acao=click&linha={$l}&coluna={$c}\"'></td>";
 							} elseif ($coluna == '@') {
@@ -157,15 +220,17 @@ if($acao != 'click') {
 							} else {
 								echo "<td onclick='javascript: window.location=\"mine_rebuild.php?acao=click&linha={$l}&coluna={$c}\"'>{$coluna}</td>";
 							}
+
 						}
+
 					echo "</tr>";
+
 				}
 				
 				if (!empty($matrizComBombas) && $acao == 'click') {
 					echo "<span style='color:snow; padding: 5px 8px; background-color: grey;'>você clicou na linha {$_REQUEST['linha']}, coluna {$_REQUEST['coluna']}</span>";
-				}
-				
-				
+					echo $bombaContador;
+				}			
 
 			?>
 
@@ -182,7 +247,6 @@ if($acao != 'click') {
 
 r($matrizComBombas);
 
-
 // for ($linha = 1; $linha <= $linhas; $linha++) {
 // 	echo "<tr>";
 // 		for ($coluna = 1; $coluna <= $colunas; $coluna++) {
@@ -190,4 +254,5 @@ r($matrizComBombas);
 // 		}
 // 	echo "</tr>";
 // }
+
 ?>
